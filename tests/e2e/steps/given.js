97549f751c5c33123e09074710c3ac91679f12aa
Charlie <charlieavery123@hotmail.co.uk>
2022-12-22 09:47:52 +0000

    created first step files

---
+++ tests/e2e/steps/given.js
@@ -0,0 +1,8 @@
+const { Given } = require("@cucumber/cucumber");
+// import expect for assertion
+const { expect } = require("@playwright/test");
+
+Given("the User is on the NewText page", function () {
+  // Write code here that turns the phrase above into concrete actions
+  expect(1).toEqual(1);
+});
---
+++ tests/e2e/steps/then.js
@@ -0,0 +1,8 @@
+const { Then } = require("@cucumber/cucumber");
+// import expect for assertion
+const { expect } = require("@playwright/test");
+
+Then("they are redirected to the Text detail page", function () {
+  // Write code here that turns the phrase above into concrete actions
+  expect(1).toEqual(1);
+});
