996cbf0650c618716901b4c8cb5ab26830c41efe
Charlie <charlieavery123@hotmail.co.uk>
2022-12-23 16:27:11 +0000

    added test conditions

--- tests/create-texts.spec.ts
+++ tests/create-texts.spec.ts
@@ -1,14 +1,19 @@
 import { test, expect } from '@playwright/test'
 
+let text = "Test Text"
+
 test('', async ({ page }) => {
     // Given the User is on the NewText page
     await page.goto('/texts/new/');
     await expect(page).toHaveTitle(/New Remix App/);
     
-    // When they click Submit button
-    await page.click('button[type="submit"]');
+    // When they fill Text content
+    // And they click Submit button
+    await page.locator('input[type="text"]').fill(text);
+    await page.locator('button[type="submit"]').click();
     
     // Then they are redirected to the Text detail page
+    // And they see the new text
     await expect(page).toHaveURL(/texts\/1/);
-
+    await expect(page).toContain(text);
   });
