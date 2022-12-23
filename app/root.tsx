8b92dca5ac5d80b89e5fc68eea9b2d15a521b338
Charlie <charlieavery123@hotmail.co.uk>
2022-12-23 16:39:28 +0000

    amended root to only live reload during development

--- app/root.tsx
+++ app/root.tsx
@@ -10,9 +10,9 @@ import {
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "Linguify",
   viewport: "width=device-width,initial-scale=1",
-});
+}); 
 
 export default function App() {
   return (
@@ -25,7 +25,7 @@ export default function App() {
         <Outlet />
         <ScrollRestoration />
         <Scripts />
-        <LiveReload />
+        {process.env.NODE_ENV === 'development' ? <LiveReload /> : null}
       </body>
     </html>
   );
