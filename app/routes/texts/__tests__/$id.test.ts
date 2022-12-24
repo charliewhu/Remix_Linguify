40cfd0a3f681ee50b5b4ddf213aa5487f8f4c622
Charlie <charlieavery123@hotmail.co.uk>
2022-12-24 13:26:08 +0000

    adapted test assertion

--- app/routes/texts/__tests__/$id.test.ts
+++ app/routes/texts/__tests__/$id.test.ts
@@ -1,10 +1,10 @@
 import { loader } from '../$id';
 
 describe('Loader', () => {
-  it('should return data from API', () => {
+  it('should return data from API', async () => {
     const request = new Request('http://fake.api/1/');
-    const response = loader({request, context: {}, params: { id: "1" } });
+    const response = await loader({request, context: {}, params: { id: "1" } });
 
-    expect(response).toEqual(2)
+    expect(response).toEqual({id: 1, name: "name", body: "body"})
   })
 })
