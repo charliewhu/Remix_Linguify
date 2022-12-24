b99eeab410f74c5a671fe85331dccbd8caea91f6
Charlie <charlieavery123@hotmail.co.uk>
2022-12-24 17:25:49 +0000

    added intercept for url

--- tests/create-texts.spec.ts
+++ tests/create-texts.spec.ts
@@ -1,24 +1,36 @@
 import { test, expect } from '@playwright/test'
 
+let id = 1
 let name = "Name"
 let body = "Body"
 
+let data = {
+  id: id,
+  name: name,
+  body: body,
+}
+
 test('', async ({ page }) => {
-    // Given the User is on the NewText page
-    await page.goto('/texts/create/');
-    await expect(page).toHaveTitle(/New Text/);
-    
-    // When they fill Name content
-    // And they fill Body content
-    // And they click Submit button
-    await page.getByPlaceholder("name").fill(name);
-    await page.getByPlaceholder("body").fill(body);
-    await page.locator('button[type="submit"]').click();
-    
-    // Then they are redirected to the Text detail page
-    // And they see the new text name
-    // And they see the new text body
-    await expect(page).toHaveURL(/texts\/1/);
-    await expect(page).toContain(name);
-    await expect(page).toContain(body);
-  });
+  await page.route('linguify.up.railway.app/texts/1/', route => route.fulfill({
+    status: 200,
+    body: JSON.stringify(data),
+  }));
+
+  // Given the User is on the NewText page
+  await page.goto('/texts/create/');
+  await expect(page).toHaveTitle(/New Text/);
+  
+  // When they fill Name content
+  // And they fill Body content
+  // And they click Submit button
+  await page.getByPlaceholder("name").fill(name);
+  await page.getByPlaceholder("body").fill(body);
+  await page.locator('button[type="submit"]').click();
+  
+  // Then they are redirected to the Text detail page
+  // And they see the new text name
+  // And they see the new text body
+  await expect(page).toHaveURL(/texts\/1/);
+  await expect(page).toContain(name);
+  await expect(page).toContain(body);
+});
