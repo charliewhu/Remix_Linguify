17153ae87a8b9d2bce8051c1ff4817271b609163
Charlie <charlieavery123@hotmail.co.uk>
2022-12-24 11:41:34 +0000

    setup vitest for unit tests

---
+++ vitest.config.ts
@@ -0,0 +1,17 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { configDefaults } from 'vitest/config'
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  test: {
+    globals: true,
+    environment: "happy-dom",
+    exclude: [...configDefaults.exclude, '**/*.spec.*'],
+    setupFiles: ["./tests/setup-test-env.ts"],
+  },
+});
