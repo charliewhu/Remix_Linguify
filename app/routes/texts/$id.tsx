298de8be3f1bccb3b44d3f4b2caf2c1ef3a34dc2
Charlie <charlieavery123@hotmail.co.uk>
2022-12-24 13:27:37 +0000

    use api request from services module

--- app/routes/texts/$id.tsx
+++ app/routes/texts/$id.tsx
@@ -1,14 +1,11 @@
 import type { LoaderFunction } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react"
+import {getText} from "../../../services/texts"
 
 type LoaderData = { id: number; name: string; body: string }
 
 export const loader: LoaderFunction = async ({ request, params }) => {
-  const data: LoaderData = {
-    id: 1,
-    name: "fake",
-    body: "fake body"
-  };
+  const data: LoaderData = getText(params.id)
   return data
 }
 
