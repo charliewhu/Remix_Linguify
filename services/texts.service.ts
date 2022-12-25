08128f4a9672fa9efd766bff3adff7e2a745fece
Charlie <charlieavery123@hotmail.co.uk>
2022-12-25 13:20:24 +0000

    added handler for api post request

--- services/texts.service.ts
+++ services/texts.service.ts
@@ -1,16 +1,28 @@
 import axiosInstance from "./base.service";
 
+type textData = { name: string; body: string }
+
 async function getText(id: string | undefined){
   try {
-    const response = await axiosInstance(`/texts/${id}/`)
+    const response = await axiosInstance.get(`/texts/${id}/`)
     return response.data;
   } catch (error) {
     return error
   }
 };
 
+async function postText(data: textData) {
+  try {
+    const response = await axiosInstance.post(`/texts/`, data)
+    return response.data;
+  } catch (error) {
+    return error
+  }
+}
+
 const textService = {
   getText,
+  postText,
 }
 
 export default textService;
