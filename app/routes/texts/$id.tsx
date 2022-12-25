608ff0791bb91364cd18b2d1ec87635555b3990d
Charlie <charlieavery123@hotmail.co.uk>
2022-12-25 08:01:32 +0000

    changed param id type

--- app/routes/texts/$id.tsx
+++ app/routes/texts/$id.tsx
@@ -5,13 +5,12 @@ import textService from "../../../services/texts.service"
 type LoaderData = { id: number; name: string; body: string }
 
 export const loader: LoaderFunction = async ({ request, params }) => {
-  const data: LoaderData = await textService.getText(+params.id)
+  const data: LoaderData = await textService.getText(params.id)
   return data
 }
 
 export default function TextDetail() {
   const data = useLoaderData<LoaderData>()
-  console.log(data)
 
   return (
     <div>
