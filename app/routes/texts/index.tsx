3b6b667d2762563093cac9d80988367dd90a0599
Charlie <charlieavery123@hotmail.co.uk>
2022-12-26 18:30:56 +0000

    wrote loader function

--- app/routes/texts/index.tsx
+++ app/routes/texts/index.tsx
@@ -1,16 +1,27 @@
-import type { MetaFunction} from "@remix-run/node";
+import type { LoaderFunction, MetaFunction} from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
 import textService from "services/texts.service";
+
+type LoaderData = { id: number; name: string; body: string }
 
 export const meta: MetaFunction = () => ({
   title: "My Texts",
 });
 
+export const loader: LoaderFunction = async ({request}) => {
+  const data: Array<LoaderData> = await textService.getTexts()
+  return data
+}
+
 export default function TextList() {
+  const data = useLoaderData<Array<LoaderData>>()
+  
   return (
     <div>
-        {textService.map(text => (
-            <li key={text.id}>{text.name}</li>
-        ))}
+      {data.map(text => (
+        <li key={text.id}>{text.name}</li>
+      ))}
     </div>
   )
 }
+    
