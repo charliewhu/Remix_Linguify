bb68b051ee0b0cd776a662b42f3b1b3efbbc7015
Charlie <charlieavery123@hotmail.co.uk>
2022-12-27 12:03:46 +0000

    changed dev server port for e2e tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -58,7 +58,7 @@ const config: PlaywrightTestConfig = {
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'yarn dev',
-    port: 3000,
+    port: 3001,
   },
 };
 
